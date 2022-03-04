67d4cb7a5d1bdb5b3935d3dae89e5ef5ccdb3db9
Kylian Lavergne <inklay@outlook.fr>
2022-03-04 11:50:28 +0100

    [ADD]: headr shadow box

--- source/UI/Header.cpp
+++ source/UI/Header.cpp
@@ -6,5 +6,7 @@ m_text(Text(Vec3(), text)) {}
 
 void Header::draw_lines(void) {
 	C2D_DrawRectangle(0, 0, 0, 400, 30, colors::header_top, colors::header_top, colors::header_bottom, colors::header_bottom);
+	C2D_DrawRectangle(0, 30, 0, 400, 20, colors::header_shadow_top, colors::header_shadow_top,
+		colors::header_shadow_bottom, colors::header_shadow_bottom);
 	m_text.draw_lines(Vec2(400, 30));
 }
