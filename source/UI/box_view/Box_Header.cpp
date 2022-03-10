09612522a6ae30382c507f06bd8214cd17ab61db
Kylian Lavergne <inklay@outlook.fr>
2022-03-10 22:18:44 +0100

    [UPD]: L & R button on box view

--- source/UI/box_view/Box_Header.cpp
+++ source/UI/box_view/Box_Header.cpp
@@ -1,4 +1,4 @@
-#include "./Box_header.hpp"
+﻿#include "./Box_header.hpp"
 
 Box_Header::Box_Header(std::string text, bool is_selected, int x_fix) : 
 m_is_selected(is_selected),
@@ -6,8 +6,8 @@ m_x_fix(x_fix),
 m_box_name(Button(Vec3(120 + m_x_fix, 25, 0), Vec2(80, 20), text)), 
 m_left(Text(Vec3(30 + m_x_fix, 20, 0), "<")),
 m_right(Text(Vec3(270 + m_x_fix, 20, 0), ">")),
-m_l(Text(Vec3(40 + m_x_fix, 20, 0), "l")),
-m_r(Text(Vec3(260 + m_x_fix, 20, 0), "r")) {
+m_l(Text(Vec3(40 + m_x_fix, 20, 0), "")),
+m_r(Text(Vec3(260 + m_x_fix, 20, 0), "")) {
 }
 
 void Box_Header::draw_lines(void) {
